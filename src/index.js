b8a99cf72e3c833b44813c46469a939209e0d36d
Muhaimeen Chowdhury <schy92@gmail.com>
2023-12-14 20:15:08 -0500

    deploy 1214

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./App/home.js";
-
+console.log(window.location.pathname);
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/*" element={<Home />} />
-        <Route path="/:urlParam/*" element={<Home />} />
+        <Route path="/:urlParam" element={<Home />} />
       </Routes>
     </Router>
   </React.StrictMode>,
