41c7905984424129894a25dd9d95eca31a25f65d
Muhaimeen Chowdhury <schy92@gmail.com>
2023-12-20 21:34:44 -0500

    hash-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Change HashRouter to BrowserRouter
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Change HashRouter to BrowserRouter
 import Home from "./App/home.js";
 import PrivacyPolicy from "./privacyPolicy.js";
 
